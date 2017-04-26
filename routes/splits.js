8a0097496b5670166608fc7555088d892ff88eea
Ketler13 <ketler13@yandex.ru>
2017-04-26 11:53:58 +0300

    add config updates

--- routes/splits.js
+++ routes/splits.js
@@ -5,7 +5,6 @@ const Split = require('../models/split');
 exports.post = async (ctx, next) => {
   await passport.authenticate('jwt', {session: false})(ctx, next);
   const {user} = ctx.request.body;
-  console.log(ctx.request.body);
   if (!user) ctx.throw(401);
   const splits = await Split.find({user});
   ctx.body = splits.map(split => {
